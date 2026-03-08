5e5879c797a885b9df011739302a693d9af5041d
Elias El Abed <eli_elabed@hotmail.fr>
2026-03-08 13:39:03 +0100

    mdr check et argument valid main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <cstdlib>
+#include <cerrno>
+#include <climits>
+
+bool isValidPort(const std::string &portStr, int &port)
+{
+	char *endptr;
+	long val = std::strtol(portStr.c_str(), &endptr, 10);
+	
+	if (*endptr != '\0' || endptr == portStr.c_str())
+		return false;
+	if (val < 1 || val > 65535)
+		return false;
+	port = static_cast<int>(val);
+	return true;
+}
 
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	std::cout << "IRC Server" << std::endl;
+	if (argc != 3)
+	{
+		std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
+		return 1;
+	}
+
+	int port;
+	if (!isValidPort(argv[1], port))
+	{
+		std::cerr << "Error: Invalid port (must be 1-65535)" << std::endl;
+		return 1;
+	}
+
+	std::string password = argv[2];
+	if (password.empty())
+	{
+		std::cerr << "Error: Password cannot be empty" << std::endl;
+		return 1;
+	}
+
+	std::cout << "Starting IRC Server on port " << port << std::endl;
+	
 	return 0;
 }
