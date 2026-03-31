24cb557cd5e260d6cc79f14434fe932b5f7eb8bd
Elias El Abed <eli_elabed@hotmail.fr>
2026-03-31 09:59:48 +0200

    server

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -23,6 +23,15 @@ private:
 	void setNonBlocking(int fd);
 	std::string extractCommand(std::string &buffer);
 	void processCommand(Client *client, const std::string &command);
+	
+	void handlePass(Client *client, const std::string &args);
+	void handleNick(Client *client, const std::string &args);
+	void handleUser(Client *client, const std::string &args);
+	void handlePing(Client *client, const std::string &args);
+	void handleQuit(Client *client, const std::string &args);
+	
+	std::vector<std::string> splitCommand(const std::string &command);
+	bool isNicknameInUse(const std::string &nickname);
 
 public:
 	Server(int port, const std::string &password);
